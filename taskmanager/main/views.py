6739937cb1f47b486e69a0fbe654c4e6a3b3adad
VasyaChopovenko <vasyachopovenko@gmail.com>
2020-12-08 19:03:52 +0300

    Fix output data

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -7,9 +7,9 @@ def index(request):
     if request.method == "POST":
         userform = UserForm(request.POST)
         if userform.is_valid():
-            login = userform.cleaned_data["cardNumber"]
+            card_number = userform.cleaned_data["cardNumber"]
             password = userform.cleaned_data["password"]
-            return HttpResponse("<h2>Пароль ({0}) для карти № {1}</h2>".format(login, password))
+            return HttpResponse("<h2>Пароль ({1}) для карти № {0}</h2>".format(card_number, password))
         else:
             return HttpResponse("Неправильні дані")
     else:
